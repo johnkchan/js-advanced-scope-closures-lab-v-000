70dd1eff4dc59bddc498df5f73e8d85c5e5200a9
Jonathan Chan <johnkchan252@gmail.com>
2019-06-12 14:05:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange) {
   return function(a, b) {
     let iA = parseInt(a.slice(0, 2), 10);
     let iB = parseInt(b.slice(0, 2), 10);
-    if (b - a) {
+    if (b - a <= blockRange) {
       
     }
   };
