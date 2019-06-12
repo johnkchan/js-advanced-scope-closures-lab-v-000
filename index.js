549589f18b75610fcc45f0fd10cb052cfcdf8e53
Jonathan Chan <johnkchan252@gmail.com>
2019-06-12 14:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function produceDrivingRange(blockRange) {
   return function(a, b) {
     let iA = parseInt(a.slice(0, 2), 10);
     let iB = parseInt(b.slice(0, 2), 10);
-    if (b - a <= blockRange) {
-      return blockRange - (b - a);
+    if (iB - iA <= blockRange) {
+      return 'within range by ' + (blockRange - (iB - iA)).toString(10);
     } else {
-      
+      return (iB - (iA + blockRange)).toString(10) + ' blocks out of range';
     }
   };
 }
